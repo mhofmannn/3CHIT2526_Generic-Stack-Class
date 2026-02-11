5fcc105fee038e3fbcd0684ae3aba562c2a658d1
mhofmannn <mhofmann@student.tgm.ac.at>
2026-02-11 10:49:04 +0100

    Add custom stack exceptions

--- src/exception/StackEmptyException.java
+++ src/exception/StackEmptyException.java
@@ -1,4 +1,14 @@
 package exception;
 
-public class StackEmptyException {
+/**
+ * Exception thrown when attempting to pop
+ * or peek from an empty stack.
+ */
+public class StackEmptyException extends Exception {
+    /**
+     * Creates a StackEmptyException with a default message.
+     */
+    public StackEmptyException() {
+        super("Stack is empty. No elements available.");
+    }
 }
--- src/exception/StackFullException.java
+++ src/exception/StackFullException.java
@@ -1,4 +1,14 @@
 package exception;
 
-public class StackFullException {
+/**
+ * Exception thrown when attempting to push
+ * an element onto a full stack.
+ */
+public class StackFullException extends Exception {
+    /**
+     * Creates a StackFullException with a default message.
+     */
+    public StackFullException() {
+        super("Stack is full. Cannot push new element.");
+    }
 }
